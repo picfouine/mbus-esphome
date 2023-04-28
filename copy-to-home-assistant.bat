6046db186501730e77941fd2990a9d3bb982b824
Paul de Jong <paul.m.de.jong@gmail.com>
2023-04-28 10:08:16 +0200

    Batch script to copy files to HomeAssistant

---
+++ copy-to-home-assistant.bat
@@ -0,0 +1,16 @@
+@echo off
+set targetDir=z:\esphome
+
+REM YML file
+set ymlFile=.\warmte-meter-mbus.yml
+copy %ymlFile% %targetDir%
+
+REM Component files
+set customComponentsSrcDir=.\custom-components\warmtemetermbus
+set customComponentsTargetDir=%targetDir%\custom_components\warmtemetermbus
+copy %customComponentsSrcDir%\__init__.py %customComponentsTargetDir%
+copy %customComponentsSrcDir%\HeatMeterMbus.cpp %customComponentsTargetDir%
+copy %customComponentsSrcDir%\HeatMeterMbus.h %customComponentsTargetDir%
+copy %customComponentsSrcDir%\Kamstrup303WA02.cpp %customComponentsTargetDir%
+copy %customComponentsSrcDir%\Kamstrup303WA02.h %customComponentsTargetDir%
+copy %customComponentsSrcDir%\sensor.py %customComponentsTargetDir%
