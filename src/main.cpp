dcfc0e41a1301852c856c6fe71f4c69833e1635e
Paul de Jong <paul.m.de.jong@gmail.com>
2023-09-16 10:01:37 +0200

    Empty main.cpp and include note

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,15 @@
+// NOTE: This file is NOT used!
+// It is merely here as part of the PlatformIO project structure.
+// It is not relevant for the ESPHome integration.
+
  #ifndef UNIT_TEST
 
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
 
 #endif // UNIT_TEST
